191c6728056c13cf9016b3743235c3ba4d520c4b
jctaoo <jctaoo@outlook.com>
2020-10-06 19:36:05 +0800

    修复：在 updateToken 时修改过期时间

--- services/user/token_store.go
+++ services/user/token_store.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"github.com/go-redis/redis/v8"
 	"gorm.io/gorm"
+	"time"
 )
 
 // 存储用户 Token 以支持灵活的基于 Token 的操作，如支持后期的可能出现的 OAuth 需求，以及 Token 的颁发与撤回功能
@@ -25,7 +26,7 @@ type TokenStore interface {
 
 // 该实现采用 Redis 来实现 Token 的存储
 type TokenStoreImpl struct {
-	db *gorm.DB
+	db  *gorm.DB
 	rdb *redis.Client
 }
 
@@ -41,6 +42,7 @@ func (t TokenStoreImpl) addNewTokenToUser(token *models.TokenAuth, user *models.
 func (t TokenStoreImpl) updateToken(tokenAuth *models.TokenAuth, ip string, newToken string) {
 	tokenAuth.TokenStr = newToken
 	tokenAuth.Ip = ip
+	tokenAuth.ExpireTime = time.Unix(time.Now().Unix()+config.SiteConfig.AuthTokenExpiresTime, 0)
 	t.db.Save(tokenAuth)
 }
 
